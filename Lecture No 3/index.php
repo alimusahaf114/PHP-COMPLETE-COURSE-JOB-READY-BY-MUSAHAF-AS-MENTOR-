dacff4183d7501bf8a8c18f5c2b9e7f63bb382e7
Musahaf Ali <musahafali43@gmail.com>
2026-04-26 13:25:34 +0500

    added class 4 code

---
+++ Lecture No 3/index.php
@@ -0,0 +1,57 @@
+<?php
+
+// echo "Hello I am Working";
+// $name = "Musahaf";
+// $age = 23;
+// $result = "FAIL";
+// echo "Hello My name is ".$name ;
+// echo "<br />";
+// echo "and my age is ".$age;
+// echo "<br />";
+// echo "I am ".$result." in exams" ;
+
+// $name1 = "Ali";
+// $2name = "Arham";
+// $name3 = "Abbas";
+
+// echo $name1;
+// echo $2name;
+// echo $name3;
+
+// $first_Name = "Arham";
+// $last_Name = "Abbas";
+
+// // echo $first_Name . $last_Name ;
+// echo "My name is $first_Name $last_Name";
+// $_name = "Ali";
+// echo $_name;
+// $PI = 3.14;
+// $PI= 5.3;
+// define('PI',3.14);
+// define('pass',false);
+// PI=4.5;
+// echo pass;
+
+// $age = 45 ;
+// echo gettype($age) ;
+// echo "<br />";
+// $name = "Ali";
+// echo gettype($name) ;
+// echo "<br />";
+// $isPass = false;
+// echo gettype($isPass) ;
+// echo "<br />";
+// $pi = 3.14;
+// echo gettype($pi) ;
+// echo "<br />";
+// $result = null;
+// echo gettype($result) ;
+
+$name = "Ali";
+echo $name;
+echo "<br />";
+// echo gettype($name);
+$age = 45;
+$result = "I am fail in exams";
+echo var_dump($result)
+?>
