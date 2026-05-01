3f54914bf7ba42ce5e3e1bf97b5bddbf5a63e1f1
Musahaf Ali <musahafali43@gmail.com>
2026-05-01 11:02:53 +0500

    I have made video of Arithmatic operators and all video code and practice code is here

---
+++ Lecture No 4/ArithmaticTask.php
@@ -0,0 +1,12 @@
+<?php
+
+$a = 20 ;
+$b = 3;
+
+$sum = $a + $b ;
+$difference = $a - $b ;
+echo "The Sum = $sum" ;
+echo "<br />";
+echo "The Difference  = $difference" ;
+
+?>
---
+++ Lecture No 4/Arithmatic_Op.php
@@ -0,0 +1,34 @@
+<?php
+
+// echo "Workuing.....";
+/***************** Arithmatic Operators ******* */
+
+// $x = 53 ;
+// $y = 5 ;
+// $sum = $x + $y ;
+// echo "The Sum of $x + $y = $sum" ;
+// echo "<br/>";
+
+// $subs = $x - $y ;
+// echo "The Substraction of $x - $y = $subs" ;
+// echo "<br/>";
+
+// $multi = $x * $y ;
+// echo "The Multiplication of $x * $y = $multi" ;
+// echo "<br/>";
+
+// $divs = $x / $y ;
+// echo "The division of $x / $y = $divs" ;
+// echo "<br/>";
+
+// $reminder = $x % $y ;
+// echo "The Reminder of $x % $y = $reminder" ;
+// echo "<br/>";
+
+// $base = 10 ;
+// $power = 3 ;
+// $result = $base ** $power ;
+// echo "$base ** $power =  $result" ;
+
+
+?>
