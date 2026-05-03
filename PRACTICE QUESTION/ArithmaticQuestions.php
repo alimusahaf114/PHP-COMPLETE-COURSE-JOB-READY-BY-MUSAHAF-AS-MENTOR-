889f24db4ba38b13297d750bc5bf8cf91f86f881
Musahaf Ali <musahafali43@gmail.com>
2026-05-03 09:45:14 +0500

    added all code of practice

---
+++ PRACTICE QUESTION/ArithmaticQuestions.php
@@ -0,0 +1,48 @@
+<?php
+
+// ************  Celsius to Farhenheit 
+
+// $celsius = 40;
+// $Fahrenheit = NULL ;
+
+// $Fahrenheit = ($celsius * 9/5) + 32 ;
+// echo "$celsius C = $Fahrenheit F";
+
+// ************ fahrenheit to celcius
+// $Fahrenheit = 104 ;
+// $celsius = NULL;
+
+// $celsius = ($Fahrenheit - 32) * 5/9 ;
+// echo "$Fahrenheit F = $celsius C";
+
+//********* Kinetic Energy */
+// $mass = 1200 ;
+// $velocity = 25 ;
+// $KE = NULL;
+
+// $KE = 0.5 * $mass * $velocity**2 ;
+// echo "Kinetic Energy = $KE"
+
+
+// potential Energy*****
+// $mass = 50;
+// $height = 12 ;
+// // $gravity = 9.8;
+// define('gravity' ,9.8);
+// $PE = NULL ;
+
+
+// $PE = $mass * $gravity * $height;
+// echo "PE = $PE"
+
+// calculate final velocity 
+
+$init_Velocity = 0 ;
+$acc = 3 ;
+$time = 8 ;
+$final_Velocity = NULL;
+
+
+$final_Velocity = $init_Velocity + $acc * $time ;
+echo "Final velcity = $final_Velocity m/s";
+?>
