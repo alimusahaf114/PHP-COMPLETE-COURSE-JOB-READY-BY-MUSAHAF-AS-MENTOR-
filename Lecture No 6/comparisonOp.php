fcf1fadb2b58c2baeccda4b775c813ded3f8bdd4
Musahaf Ali <musahafali43@gmail.com>
2026-05-14 10:27:22 +0500

    added code

---
+++ Lecture No 6/comparisonOp.php
@@ -0,0 +1,21 @@
+<?php
+// echo "Working";
+// echo (3 < 4);
+// echo (7 > 4);
+// echo (10 > 30);
+// echo (10 <= 10)
+// echo (4 == '4')
+// echo (4 === '4')
+// echo (3 !== '3')
+
+// $name = "mouse";
+// echo ($name == "mouse")
+// $isPass = true;
+// echo ($isPass == 1)
+// $a = 10 ;
+// $b = '10' ;
+
+// echo ($a < $b)
+// echo ($a === $b)
+// echo ($a !== $b)
+?>
---
+++ Lecture No 7/logicalOp.php
@@ -0,0 +1,20 @@
+<?php
+
+// echo "Yes I am working";
+// echo (13>5);
+// echo (2 > 1 && 5 > 10)// true and false 
+// echo (2 > 1 && 50 > 10)// true and true 
+// echo (20 > 100 && 50 > 100)// false and false 
+// echo (20 > 1 && 50 < 100 && 100 > 20)// true and true and true
+// echo (20 > 1 and 50 < 100 and 100 > 20)// true and true and true
+
+
+// echo (10 > 20 || 40 < 10)
+// echo ( (20 > 10 && 100 > 200) || 100 > 200 )
+
+// echo (!true);
+// echo (!false);
+// echo (!(3>10))
+echo (!(10 > 20 && 30 > 100))
+
+?>
