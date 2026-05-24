8b42a2182ce25bfdf1d5a79261e0ab660c644092
Musahaf Ali <musahafali43@gmail.com>
2026-05-24 15:15:09 +0500

    added class 9 lecture code

---
+++ Lecture No 9/incDec.php
@@ -0,0 +1,32 @@
+<?php
+
+// echo "Hello world";
+// $x = 3;
+// $x = 4;
+// $x += 1;
+// $x = $x + 1;
+// $x++;
+// echo $x++; //post increment
+// echo ++$x;
+// $x++;
+// $x++;
+// echo ++$x; //pre increment 6
+// echo $x++; //post increment 5
+// echo "<br/>";
+// echo $x;
+
+// $x = 3 ;
+// echo $x++ + ++$x;
+// echo $x++ + $x++ + $x++; 
+// echo ++$x + $x++;
+
+
+$x = 5 ;
+// $x--; //post descrement
+// echo $x--;
+// echo $x;
+// echo --$x; 
+
+// echo --$x + ++$x;
+
+?>
---
+++ Practice/index.php
@@ -0,0 +1,19 @@
+<?php
+// echo "Hello world";
+// $a = [12] ;
+// settype($a , "double");
+// echo gettype($a)
+// settype()
+// echo isset($a)
+// echo is_array($a);
+// $a = 12;
+// unset($a);
+// echo $a;
+
+// $a = "Hello";
+// $b = $a;
+// unset($b);
+// echo $a;
+// echo "<br/>";
+// echo $b;
+?>
