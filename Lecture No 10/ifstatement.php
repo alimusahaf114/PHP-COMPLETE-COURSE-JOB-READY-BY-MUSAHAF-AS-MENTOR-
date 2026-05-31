9124057d8b91b0b229004280158495cbc6fa8c25
Musahaf Ali <musahafali43@gmail.com>
2026-05-31 09:50:04 +0500

    added my all files code related to lecture no 10

---
+++ Lecture No 10/ifstatement.php
@@ -0,0 +1,28 @@
+<?php
+echo "<h1> If Statement </h1>";
+//  echo "Hello world";
+/*
+if(condition){
+code
+}
+
+*/
+// $age = 10;
+// if($age >= 18){
+//     echo "You are adult";
+// }
+// $name = "Musahaf";
+// if(FALSE){
+//     echo "You are invited ";
+// }
+// $isPass = FALSE;
+// if($isPass){
+//     echo "You are eligible to Promote in next class";
+// }
+
+$age = 18 ;
+$city = "Karachi";
+if($age >= 18 && $city == "Hyderabad"){
+    echo "You are eligible to Promote in next class";
+}
+?>
