b08882c0f968d5650fc70fdeee003d6f70ae58df
Musahaf Ali <musahafali43@gmail.com>
2026-06-05 10:04:33 +0500

    added switch statment code

---
+++ Lecture No 14/switchStatement.php
@@ -0,0 +1,52 @@
+<?php
+//  echo "Hello world";
+// $marks = 80;
+// switch(80){
+//     case 90:
+//         echo "A+ Grade";
+//         break;
+//     case 80:
+//         echo "B+ Grade";
+//         break;
+//     case 70;
+//         echo "C+ Grade";
+//         break;
+//     default:
+//         echo "You are Fail";
+// }
+
+// switch(5){
+//     case 1:
+//         echo "Monday";
+//         break;
+//     case 2:
+//         echo "Tuesday";
+//         break;
+//     case 3:
+//         echo "Wednesday";
+//         break;
+//     case 4:
+//         echo "Thursday";
+//         break;
+//     case 5:
+//         echo "Friday";
+//         break;
+//     case 6:
+//         echo "Saturday";
+//         break;
+//     case 7:
+//         echo "Sunday";
+//         break;
+//     default :
+//         echo "Invalid Day";
+// }
+
+switch(30 > 6){
+    case 4 > 2: 
+        echo "Hello world";
+        break;
+    case 2 :
+        echo "Bye World";
+        break;
+}
+?>
